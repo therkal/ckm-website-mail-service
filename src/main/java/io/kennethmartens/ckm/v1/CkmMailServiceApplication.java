705ab22fe2d990973c51fbb166791152190ee082
Kenneth Martens <kenneth.martens@proton.me>
2022-10-27 12:49:49 -0500

    Updates files

--- src/main/java/io/kennethmartens/ckm/v1/CkmMailServiceApplication.java
+++ src/main/java/io/kennethmartens/ckm/v1/CkmMailServiceApplication.java
@@ -4,5 +4,5 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 @ApplicationPath("/api/v1")
-public class CkmEmailServiceApplication extends Application {
+public class CkmMailServiceApplication extends Application {
 }
